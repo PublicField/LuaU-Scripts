4a189b57729a33652a274d357bff9bc2f97afa5b
DevRobloxian <46647421+DevRobloxian@users.noreply.github.com>
2023-05-27 18:40:17 +0300

    Update DataStoreHandlerClient.lua

--- Modules/DataStoreHandler/DataStoreHandlerClient.lua
+++ Modules/DataStoreHandler/DataStoreHandlerClient.lua
@@ -2,54 +2,55 @@
 
 local HttpService = game:GetService("HttpService")
 
+local function ConvertTableToJSON(T : {any}) : string | nil --Returns table converted to JSON as a string.
+	local success, value = pcall(HttpService.JSONEncode, HttpService, T)
+
+	if not success then
+		return nil
+	end
+
+	return value
+end
+
+local function ConvertJSONToTable(json : string) : {any} | any --Returns JSON converted to table.
+	local success, value = pcall(HttpService.JSONDecode, HttpService, json)
+
+	if not success then
+		return json
+	end
+
+	return value
+end
+
 local ClientDataStoreHandler = {}
+ClientDataStoreHandler.__index = ClientDataStoreHandler
 
-function ClientDataStoreHandler:Init(replicatedInstance : Instance)
-	if not replicatedInstance or typeof(replicatedInstance) ~= "Instance" then
+function ClientDataStoreHandler.new(replicatedInstance : Instance)
+	if typeof(replicatedInstance) ~= "Instance" then
 		error("Replicated instance is invalid and cannot be used.")
-		return false
 	end
-	
+
 	local self = {}
-	
-	function self:ConvertTableToJSON(T : {any}) : string | nil --Returns table converted to JSON as a string.
-		local success, value = pcall(HttpService.JSONEncode, HttpService, T)
+	self._replicatedInstance = replicatedInstance
 
-		if not success then
-			return nil
-		end
+	return setmetatable(self, ClientDataStoreHandler)
+end
 
-		return value
+function ClientDataStoreHandler:Get(index : string) --Gets data from replicated instance. Converts JSONs to table.
+	local Attribute = ConvertJSONToTable(self._replicatedInstance:GetAttribute(index))
+
+	return Attribute
+end
+
+function ClientDataStoreHandler:RetrieveData() : {any} --Returns table with replicated instance data and converts JSONs to tables if possible.
+	local Attributes = self._replicatedInstance:GetAttributes()
+	local RetrievedData = {}
+
+	for i,v in Attributes do
+		RetrievedData[i] = self:Get(i)
 	end
 
-	function self:ConvertJSONToTable(json : string) : {any} | any --Returns JSON converted to table.
-		local success, value = pcall(HttpService.JSONDecode, HttpService, json)
-
-		if not success then
-			return json
-		end
-
-		return value
-	end
-	
-	function self:Get(index : string) --Gets data from replicated instance. Converts JSONs to table.
-		local Attribute = self:ConvertJSONToTable(replicatedInstance:GetAttribute(index))
-
-		return Attribute
-	end
-	
-	function self:RetrieveData() : {any} --Returns table with replicated instance data and converts JSONs to tables if possible.
-		local Attributes = replicatedInstance:GetAttributes()
-		local RetrievedData = {}
-
-		for i,v in Attributes do
-			RetrievedData[i] = self:Get(i)
-		end
-
-		return RetrievedData
-	end
-	
-	return self
+	return RetrievedData
 end
 
 return ClientDataStoreHandler
